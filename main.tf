d389501aaa6b3fedaae90d53cf7b5d1f1a52f5e0
masa3 <masa@hashicorp.com>
2019-07-10 13:08:56 +0900

    commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "var1" {
+	default = "foo"
+}
+
+resource "null_resource" "test" {
+
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    command = "touch file"
+  }
+}
+
+output "sample_var" {
+	value = var.var1
+}
