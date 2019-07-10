7ebc6cf917e606d117394a03851273a22b74465f
masatomo-ito2 <45160975+masatomo-ito2@users.noreply.github.com>
2019-07-10 16:49:50 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
-variable "var1" {
-	default = "foo"
-}
-
 resource "null_resource" "test" {
 
   provisioner "local-exec" {
     # Bootstrap script called with private_ip of each node in the clutser
-    command = "touch file"
+    command = "/bin/sh -c env"
   }
-}
-
-output "sample_var" {
-	value = var.var1
 }
 
 output "ws" {
