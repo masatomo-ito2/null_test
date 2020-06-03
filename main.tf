aacde38ec3c1102f00afab1fea8db6b83c23eb8a
masa3 <masa@hashicorp.com>
2020-06-03 15:22:10 +0900

    test

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 variable "ATLAS_WORKSPACE_NAME" {}
+variable "TF_VAR_ATLAS_WORKSPACE_NAME" {}
+variable "API_KEY" {}
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
     # Bootstrap script called with private_ip of each node in the clutser
     command = "/bin/sh -c env"
   }
+}
+
+resource "null_resource" "addition" {
+	provisioner "local-exec" {
+		command = "/bin/sh -c hostname"
+	}
 }
 
 output "ws1" {
@@ -14,3 +22,16 @@ output "ws1" {
 output "ws2" {
 	value = var.ATLAS_WORKSPACE_NAME
 }
+
+output "ws3" {
+	value = var.TF_VAR_ATLAS_WORKSPACE_NAME
+}
+
+output "null_id" {
+	value = null_resource.addition.id
+}
+
+output "api_key" {
+	value = var.API_KEY
+}
+
